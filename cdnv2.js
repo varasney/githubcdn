cc700490361dcb028aa2c65c363af951a0d1b48b
varasney <varasney@gmail.com>
2021-07-01 18:27:52 +0530

    Update and rename cdnv1.js to cdnv2.js

--- cdnv2.js
+++ cdnv2.js
@@ -1,5 +1,5 @@
-document.html.style.width = "150px";
- document.html.style.maxWidth = "200px";
+document.style.width = "150px";
+ document.style.maxWidth = "200px";
 document.body.style.width = "150px";
  document.body.style.maxWidth = "200px";
  console.log('styled renderd html v1')
