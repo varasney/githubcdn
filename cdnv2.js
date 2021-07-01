08a7dc46de75056eda9a3a24214b02286bfc1827
varasney <varasney@gmail.com>
2021-07-01 18:28:54 +0530

    Update cdnv2.js

--- cdnv2.js
+++ cdnv2.js
@@ -1,5 +1,5 @@
-document.style.width = "150px";
- document.style.maxWidth = "200px";
+document.html.width = "150px";
+ document.html.maxWidth = "200px";
 document.body.style.width = "150px";
  document.body.style.maxWidth = "200px";
  console.log('styled renderd html v1')
