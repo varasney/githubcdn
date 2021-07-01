8896bd9aff48ceb2e26519a49ca7cfb7407df868
varasney <varasney@gmail.com>
2021-07-01 18:43:12 +0530

    Update and rename cdnv3.js to cdnv4.js

--- cdnv3.js
+++
@@ -1,5 +0,0 @@
-document.html.width = "150px";
- document.html.maxWidth = "200px";
-document.body.style.width = "150px";
- document.body.style.maxWidth = "200px";
- console.log('styled renderd html v1')
---
+++ cdnv4.js
@@ -0,0 +1,3 @@
+document.body.width = "150px";
+ document.body.maxWidth = "200px";
+ console.log('styled renderd html v1')
